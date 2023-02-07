ee18e98af9c428efafd9d62ae3ce87ba3f522439
bbranco <bbranco@riachuelo.com.br>
2023-02-06 21:11:07 -0300

    adiciona exercicios fiap python

---
+++ pythonFiap/exercicio8.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# coding: utf-8
+nivel=input("Digite o nível de acesso: ").upper()
+if nivel=="ADM" or nivel=="USR":
+    genero=input("Digite o seu gênero: ").upper()
+    if nivel=="ADM":
+        if genero=="FEMININO":
+            print("Olá administradora")
+        else:
+            print("Olá administrador")
+    else:
+        if genero=="FEMININO":
+            print("Olá usuária")
+        else:
+            print("Olá usuário")
+elif nivel=="GUEST":
+    print("Olá visitante")
+else:
+    print("Olá desconhecido(a)")
