c6e67f18b66c3738f3b3154f60ed05cfaf532c8c
bbranco <bbranco@riachuelo.com.br>
2023-02-24 21:55:16 -0300

    adicionando execicios de shell script

---
+++ pythonFiap/for.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+# coding: utf-8
+for numero in range(1,int(input("Digite um numero para determinar o fim: ")),1):
+    print ("	" + str(numero))
---
+++ pythonFiap/whilePython.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# coding: utf-8
+numero=int(input("Digite um numero: "))
+while numero<100:
+    print("	" + str(numero))
+    numero=numero+1
+print("Laço encerrado....")
